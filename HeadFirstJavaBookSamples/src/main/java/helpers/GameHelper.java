19b7bc14d10f3a479345761c6107130afa56645d
Ozkan S <ozkan.sari@outlook.com>
2017-03-12 17:37:53 +0200

    Headfirst samples

---
+++ HeadFirstJavaBookSamples/src/main/java/helpers/GameHelper.java
@@ -0,0 +1,17 @@
+package helpers;
+
+import java.util.ArrayList;
+
+public class GameHelper {
+
+	public String getUserInput(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<String> placeDotCom(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
